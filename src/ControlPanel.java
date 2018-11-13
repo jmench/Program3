56ad39be2e9ea3e273ed691e4d7441c5c873f5f5
maco248 <malikconner@live.com>
2018-11-13 12:59:34 -0500

    Added basic functions for ControlPanel and created the ControlPoint Class

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -3,13 +3,14 @@ import java.awt.*;
 
 public class ControlPanel extends JPanel{
 
-    public ControlPanel(){
-        setBackground (Color.BLACK);
+    public ControlPanel(Color c){
+        setBackground (c);
     }
 
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        g.setColor(Color.BLACK);
+        g.drawLine(100, 100, 400, 400);
     }
 }
--- src/View.java
+++ src/View.java
@@ -5,7 +5,7 @@ public class View extends JFrame {
     private JButton setLeft, setRight, previewMorph;
     private JSlider ControlPointSize;
     private JPanel buttonPanel, imagePanel;
-    private ControlPanel cpanel;
+    private ControlPanel leftPanel, rightPanel;
 
     public void JMorphView(){
 
@@ -13,7 +13,23 @@ public class View extends JFrame {
         //This panel is used to hold our buttons
         buttonPanel = new JPanel();
 
-        cpanel = new ControlPanel();
+        imagePanel = new JPanel();
+        FlowLayout experimentLayout = new FlowLayout();
+
+
+
+        imagePanel.setLayout(experimentLayout);
+
+        leftPanel = new ControlPanel(Color.DARK_GRAY);
+        rightPanel = new ControlPanel(Color.LIGHT_GRAY);
+
+        leftPanel.setPreferredSize(new Dimension(200, 200));
+        rightPanel.setPreferredSize(new Dimension(200, 200));
+
+        imagePanel.add(leftPanel);
+        imagePanel.add(rightPanel);
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
 
         //Allows the user to set the left and the right images
         setLeft = new JButton("Set Left Image");
@@ -25,8 +41,10 @@ public class View extends JFrame {
         buttonPanel.add(setLeft);
         buttonPanel.add(setRight);
         buttonPanel.add(previewMorph);
-        add(cpanel);
+        add(imagePanel);
         add(buttonPanel);
+
+       // cpanel.repaint();
         setSize(1000,1000);
         setVisible(true);
     }
