623e3e7e3dd16a99b74b600b87085dae7469b91c
maco248 <malikconner@live.com>
2018-11-13 19:44:38 -0500

    Draw lines between each control point

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -15,16 +15,32 @@ public class ControlPanel extends JPanel{
         g.setColor(Color.BLACK);
         for(int i=0; i<10; i++){
             for(int j=0; j<10; j++){
-                CPArray[i][j] = new ControlPoint(i*(400/9)-1, j*(300/9)-1, 5);
-               // CPList.add(new ControlPoint(i*(400/9)-1, j*(300/9)-1, 5));
+                CPArray[i][j] = new ControlPoint(i*(400/9), j*(300/9), 5);
+                System.out.println(CPArray[i][j].getPosX() + " "+ CPArray[i][j].getPosY());
                 g.fillOval(CPArray[i][j].getPosX(), CPArray[i][j].getPosY(), CPArray[i][j].getRadius(), CPArray[i][j].getRadius());
             }
         }
 
-        for(int i=0; i<CPList.size(); i++){
-            //g.fillOval(CPList.get(i).getPosX(), CPList.get(i).getPosY(), CPList.get(i).getRadius(), CPList.get(i).getRadius());
-            //g.drawLine(CPList.get());
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+
+                //Draws the diagonal connections between the points
+                if(i!=9 &&  j!=9){
+                    g.drawLine(CPArray[i][j].getPosX()+2, CPArray[i][j].getPosY()+2, CPArray[i+1][j+1].getPosX()+2, CPArray[i+1][j+1].getPosY()+2);
+                }
+
+                //Draws the vertical connections between the points
+                if(j!=9){
+                    g.drawLine(CPArray[i][j].getPosX()+2, CPArray[i][j].getPosY(), CPArray[i][j+1].getPosX()+2, CPArray[i][j+1].getPosY());
+                }
+
+                //Draws the horizontal connections between the points
+                if(i!=9){
+                    g.drawLine(CPArray[i][j].getPosX(), CPArray[i][j].getPosY()+2, CPArray[i+1][j].getPosX(), CPArray[i+1][j].getPosY()+2);
+                }
+            }
         }
+
 
     }
 }
